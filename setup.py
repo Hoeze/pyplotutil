1fedfc75895514df64055d6504e2a6fb224eaf45
Florian R. Hölzlwimmer <git.ich@frhoelzlwimmer.de>
2020-11-10 17:22:09 +0100

    initial commit

---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+requirements = [
+    "seaborn",
+]
+
+test_requirements = [
+]
+
+
+setup(
+    name='pyplotutil',
+    version='0.0.1',
+    description="Python plot utilities",
+    author="Florian Hölzlwimmer",
+    author_email='git.ich@frhoelzlwimmer.de',
+    url='https://github.com/Hoeze/pyplotutil',
+    long_description="Python plot utilities",
+    packages=find_packages(),
+    install_requires=requirements,
+    extras_require={
+        "develop": ["bumpversion",
+                    "wheel",
+                    "pytest",
+                    "pytest-pep8",
+                    "pytest-cov"],
+    },
+    license="MIT license",
+    zip_safe=False,
+    test_suite='tests',
+    tests_require=test_requirements
+)
